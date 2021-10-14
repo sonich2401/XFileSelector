f39351376d4f9c05544735cd65ce8dfc49807339
sonich2401 <sonich2401@users.noreply.github.com>
2021-10-14 13:45:08 -0700

    Change Licence

--- FileSelect.cpp
+++ FileSelect.cpp
@@ -1,23 +1,17 @@
-/*
-RenDev#2616 File Selector Tool
+/*RenDev#2616 File Selector Tool
     Copyright (C) 2021 RenDev
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+    Permission is also granted to not credit the author in any way as long as you do not take credit
+    for this piece of software.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, 
+    OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, 
+    DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS 
+    ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-Contact me at vgngamingnetwork@gmail.com if you need to contact me
-*/
+Contact me at vgngamingnetwork@gmail.com if you need to contact me about this licence*/
 #include "FileSelect.h"
 #include "utils/FileUtils.h"
 #include <string.h>
